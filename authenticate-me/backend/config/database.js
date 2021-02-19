86717190dfc86fdbbed1588a01c057761252f34b
Jamie Kichuk <jckichuk@gmail.com>
2021-02-18 21:49:27 -0600

    first commit

---
+++ authenticate-me/backend/config/database.js
@@ -0,0 +1,31 @@
+const config = require('./index')
+const db = config.db
+
+const {
+  username,
+  password,
+  database,
+  host
+} = db
+
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
