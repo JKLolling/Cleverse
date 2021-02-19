c89ef36112eaaad5dd2e1281143090af20b3bf97
Jamie Kichuk <jckichuk@gmail.com>
2021-02-19 11:26:59 -0600

    error handling messages

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const csurf = require('csurf')
 const helmet = require('helmet')
+const { ValidationError } = require('sequelize')
 const cookieParser = require('cookie-parser')
 const { environment } = require('./config')
 const routes = require('./routes')
@@ -27,7 +28,6 @@ app.use(helmet({
 app.use(
   csurf({
     cookie: {
-      name: 'e',
       secure: isProduction,
       sameSite: isProduction && 'Lax',
       httpOnly: true
@@ -36,5 +36,34 @@ app.use(
 )
 
 app.use(routes)
+
+app.use((req, res, next) => {
+  const err = new Error('The requested page could not be found')
+  err.title = 'Resource Not Found'
+  err.errors = ['The requested page could not be found']
+  err.status = 404
+  next(err)
+})
+
+// Sequelize error handler
+app.use((err, req, res, next) => {
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map(e => e.message)
+    err.title = 'Validation Error'
+  }
+  next(err)
+})
+
+// Error formatter
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  console.error(err)
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  })
+})
 
 module.exports = app
