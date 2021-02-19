4ca68da88eaaca460f6dbb0ffc6c8cd528bd86b1
Jamie Kichuk <jckichuk@gmail.com>
2021-02-19 17:29:04 -0600

    backend done

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs')
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
-router.post('/', asyncHandler(async (req, res, next) => {
+
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors,
+];
+
+
+router.post('/', validateLogin, asyncHandler(async (req, res, next) => {
   const { credential, password } = req.body
   const user = await User.login({ credential, password })
   if (user) {
@@ -40,5 +55,7 @@ router.get(
     } else return res.json({});
   }
 );
+
+
 
 module.exports = router;
--- authenticate-me/backend/routes/api/users.js
+++ authenticate-me/backend/routes/api/users.js
@@ -1,13 +1,35 @@
 // backend/routes/api/users.js
 const express = require('express')
 const asyncHandler = require('express-async-handler');
+const { check, validationResult } = require('express-validator');
 
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
-router.post('/', asyncHandler(async (req, res) => {
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors,
+];
+
+router.post('/', validateSignup, asyncHandler(async (req, res) => {
   const { username, email, password } = req.body
   const user = await User.signup(username, email, password)
   await setTokenCookie(res, user)
