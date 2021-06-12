d55494833b9d5ff9a1a8ea195e44ce49938a289e
Jamie Kichuk <jckichuk@gmail.com>
2021-06-11 19:17:03 -0500

    upgrades

--- authenticate-me/backend/config/database.js
+++ authenticate-me/backend/config/database.js
@@ -22,11 +22,5 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
   },
 };
